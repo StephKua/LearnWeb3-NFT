e4c61c6e70d6e40b05c2de9d9853c8739fd89b14
StephKua <stephenkkz97@gmail.com>
2022-05-31 23:32:42 +0800

    added contract code

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x63f9b0a9bffda30a019d69c2fbf71d0f1830356b";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
